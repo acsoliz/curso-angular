2dc26c190254084c8782ec2965af0779e4d59ad3
acsoliz <acsoliz@outlook.com>
2023-11-20 15:54:43 +0100

    Formularios reactivos parte4 (FormBuilder)

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
@@ -8,6 +8,10 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class Formulario3Component {
 
+  constructor(private fb: FormBuilder){
+
+  }
+  
   get name(){
     return this.formUser.get('name') as FormControl;
   };
@@ -16,11 +20,15 @@ export class Formulario3Component {
     return this.formUser.get('email') as FormControl;
   };
 
+  formUser = this.fb.group({
+    'name': ['', Validators.required],
+    'email': ['', [Validators.required, Validators.email]]
+  })
 
-  formUser = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-  });
+  // formUser = new FormGroup({
+  //   'name': new FormControl('', Validators.required),
+  //   'email': new FormControl('', [Validators.required, Validators.email]),
+  // });
 
   procesar(){
     console.log(this.formUser.value)
