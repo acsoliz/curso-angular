c12a38b808e2b925285708f661937a80b484e024
acsoliz <acsoliz@outlook.com>
2023-11-21 14:09:08 +0100

    Services

---
+++ src/app/list-messages/list-messages.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MessagesService } from '../messages.service';
+
+@Component({
+  selector: 'app-list-messages',
+  templateUrl: './list-messages.component.html',
+  styleUrls: ['./list-messages.component.css']
+})
+
+
+export class ListMessagesComponent {
+  constructor (public messageService : MessagesService){
+
+  }
+  
+
+}
